a6cb4a3b5b3e31869e9051acd82fce95885b7e73
Keval9247 <kevaljeetbadarakhiya@gmail.com>
2025-03-08 23:08:48 +0530

    fix: render initialized.

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 const token = localStorage.getItem('token');
 
 export const axiosInstance = axios.create({
-    baseURL: `${import.meta.env.VITE_API_BASE_URL}` + "/api",
+    baseURL: `${import.meta.env.VITE_API_BASE_URL}` ? "/api" : "https://chatty-4q79.onrender.com/api",
     headers: {
         Authorization: `Bearer ${token}`,
     },
