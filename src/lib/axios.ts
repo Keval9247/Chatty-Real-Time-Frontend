5a5df2480535809407f26bdede4fddd45932588a
Keval9247 <kevaljeetbadarakhiya@gmail.com>
2025-03-23 10:42:32 +0530

    Configure Vercel to use dist folder

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 const token = localStorage.getItem('token');
 
 export const axiosInstance = axios.create({
-    baseURL: `${import.meta.env.VITE_API_BASE_URL}` ? "/api" : "https://chatty-4q79.onrender.com/api",
+    baseURL: `${import.meta.env.VITE_API_BASE_URL}` ? "/api" : "https://api-chatty.onrender.com",
     headers: {
         Authorization: `Bearer ${token}`,
     },
