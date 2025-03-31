2b114d4b45663ed9783a4116b9cc1f3ab995d936
Keval9247 <2102031000011@silveroakuni.ac.in>
2025-03-31 17:04:03 +0530

    changes: in axiosintance file.

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,12 +1,15 @@
 import axios from "axios";
 const token = localStorage.getItem('token');
 
+// Check if VITE_API_BASE_URL is defined, and use it for production, otherwise fallback to a default backend URL
 export const axiosInstance = axios.create({
-    baseURL: `${import.meta.env.VITE_API_BASE_URL}` ? "/api" : "https://api-chatty.onrender.com",
+    baseURL: import.meta.env.VITE_API_BASE_URL
+        ? `${import.meta.env.VITE_API_BASE_URL}/api`  // Use the provided API base URL
+        : "https://api-chatty.onrender.com/api",    // Fallback to the default backend URL
     headers: {
         Authorization: `Bearer ${token}`,
     },
     timeout: 15000,
     validateStatus: (status) => status >= 200 && status < 300,
     withCredentials: true,
-})
+});
